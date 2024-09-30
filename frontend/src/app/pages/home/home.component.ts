f7c91463633eb5252da67176402d85b748ca78ee
nedim-m <73715705+nedim-m@users.noreply.github.com>
2024-09-30 20:41:10 +0200

    added leaflet for maps

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isBrowser: boolean = false; 
 
+  ngOnInit(): void {
+    this.isBrowser = typeof window !== 'undefined'; 
+
+    if (this.isBrowser) {
+      import('leaflet').then(L => {
+        const map = L.map('map').setView([43.515678, 18.307556], 13); 
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        const marker = L.marker([43.515678, 18.307556]).addTo(map);
+        marker.bindPopup("<b>Jaric d.o.o.</b><br>Naša firma se nalazi ovde.").openPopup();
+      });
+    }
+  }
 }
