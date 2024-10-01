da3fe261cbfab07e47c7aea76b69e1d83ec154ce
nedim-m <73715705+nedim-m@users.noreply.github.com>
2024-10-01 21:10:38 +0200

    Added services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
-import { ServicesComponent } from './pages/services/services.component';
+import { ServicesComponent } from './pages/servicess/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HeaderComponent } from './pages/header/header.component';
 
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
